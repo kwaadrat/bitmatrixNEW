ac47bf7ba09cf3ca58d2ae7f6e7c39acc866a14c
kwaadrat <kwaadrat@gmail.com>
2023-04-15 17:50:09 +0200

    zad1

--- bitmatrixNEW/BitMatrix1.cs
+++ bitmatrixNEW/BitMatrix1.cs
@@ -1,5 +1,26 @@
-﻿using System;
-using System.Collections;
+﻿// file: BitMatrixPartial.cs
 
-// prostokątna macierz bitów o wymiarach m x n
 public partial class BitMatrix
+{
+ public override string ToString()
+    {
+        char[][] bits = new char[NumberOfRows][];
+        int i = 0, j = 0;
+        foreach (var b in data)
+        {
+            if (bits[i] == null)
+                bits[i] = new char[NumberOfColumns];
+            bits[i][j] = (bool)b ? '1' : '0';
+            i++;
+            if (i == NumberOfRows)
+            {
+                j++;
+                i = 0;
+            }
+        }
+        string r = "";
+        foreach (var b in bits)
+            r += new string(b) + "\n";
+        return r;
+    }
+}
